03db171707bddc9fa0b26f11536aeacc36adb3e7
LAPTOP-E0FIB2AJ\trinh <trinhtam2001@gmail.com>
2025-06-17 19:46:32 +0700

    add spinner

--- src/feature/orichidViewList/orichidViewList.tsx
+++ src/feature/orichidViewList/orichidViewList.tsx
@@ -6,20 +6,23 @@ import { Orchid } from "../../model.ts/orchids"
 import CreateOrchidModal from "../../component/OrchidCard/CreateOrchidModal"
 import appLocalStorage from "../../util/appLocalStorage"
 import { localKeyItem } from "../../util/localKeyItem"
+import { Spinner } from "react-bootstrap"
 
 const OrichidViewList = () => {
     const [orchids, setOrchids] = useState<Orchid[]>();
     const [isCreateModalOpen, setIsCreateModalOpen] = useState(false);
+    const [loading, setLoading] = useState<boolean>(true)
 
     const getListOfOrchidsAsync = async () => {
         const orchids = await OrchidService.getOrchids();
         setOrchids(orchids);
+        setLoading(false)
     }
 
     const handleOnClick = () => {
         var storedCredential = appLocalStorage.getItem(localKeyItem.userCredential);
         console.log("Stored Credential: ", storedCredential);
-        
+
         if (!storedCredential) {
             alert("Please login to create a new orchid.");
             return;
@@ -34,29 +37,37 @@ const OrichidViewList = () => {
 
     return (
         <>
-            <div>
-                <div className={styles['list-header']}>
-                    <h1>Orchid Collection</h1>
-                    <button
-                        className={styles['create-button']}
-                        onClick={() => handleOnClick()}
-                    >
-                        Create New Orchid
-                    </button>
-                </div>
-                <div className={styles['list-view-container']}>
-                    {orchids?.map((orchid) => (
-                        <div className={styles['card-wrapper']} key={orchid.id}>
-                            <OrchidsCard {...orchid} />
+            {loading ?
+                (
+                    <div style={{ display: "flex", justifyContent: "center", alignItems: "center" }}>
+                        <Spinner />
+                    </div>
+                ) :
+                (
+                    <div>
+                        <div className={styles['list-header']}>
+                            <h1>Orchid Collection</h1>
+                            <button
+                                className={styles['create-button']}
+                                onClick={() => handleOnClick()}
+                            >
+                                Create New Orchid
+                            </button>
                         </div>
-                    ))}
-                </div>
-                <CreateOrchidModal
-                    isOpen={isCreateModalOpen}
-                    onClose={() => setIsCreateModalOpen(false)}
-                    onOrchidCreated={getListOfOrchidsAsync}
-                />
-            </div>
+                        <div className={styles['list-view-container']}>
+                            {orchids?.map((orchid) => (
+                                <div className={styles['card-wrapper']} key={orchid.id}>
+                                    <OrchidsCard {...orchid} />
+                                </div>
+                            ))}
+                        </div>
+                        <CreateOrchidModal
+                            isOpen={isCreateModalOpen}
+                            onClose={() => setIsCreateModalOpen(false)}
+                            onOrchidCreated={getListOfOrchidsAsync}
+                        />
+                    </div>
+                )}
         </>
 
     )
